f91d615f6400f904edd43bb4526779dbde6d9b08
abbos <abbosvaliev2003@gmail.com>
2022-10-07 21:33:59 +0500

    feat: add new page tomografiya

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -349,3 +349,21 @@ const swiperDocReview = new Swiper('.doc-review__swiper', {
   //   },
   // }
 });
+// Аккордеон
+function accordion() {
+    const items = document.querySelectorAll('.accordion__item-trigger')
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            if (parent.classList.contains('accordion__item-active')) {
+                parent.classList.remove('accordion__item-active')
+            } else {
+                document
+                    .querySelectorAll('.accordion__item')
+                    .forEach(child => child.classList.remove('accordion__item-active'))   
+                parent.classList.add('accordion__item-active')
+            }
+        })
+    })
+}
+accordion() 
