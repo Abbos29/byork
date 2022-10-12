cc25ca989954a5b4361f40cc80352fe5e15ea09d
abbos <abbosvaliev2003@gmail.com>
2022-10-12 22:47:39 +0500

    feat: fix js bags

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,6 +1,7 @@
 // Custom Scripts
 // Custom scripts
 
+
 // Мобильное меню бургер
 function burgerMenu() {
   const burger = document.querySelector('.burger')
@@ -50,48 +51,7 @@ function fixedNav() {
 }
 window.addEventListener('scroll', fixedNav)
 
-
-
-
-// TABS 
-
-function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
-    const header = document.querySelector(headerSelector),
-          tab = document.querySelectorAll(tabSelector),
-          content = document.querySelectorAll(contentSelector)
-    function hideTabContent() {
-        content.forEach(item => {
-            item.style.display = 'none'
-        });
-        tab.forEach(item => {
-            item.classList.remove(activeClass)
-        });
-    }
-    function showTabContent(i = 0) {
-       content[i].style.display = display
-       tab[i].classList.add(activeClass)
-    }
-    hideTabContent()
-    showTabContent()
-    header.addEventListener('click', e => {
-        const target = e.target
-        if (target.classList.contains(tabSelector.replace(/\./, '')) || 
-        target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
-            tab.forEach((item, i) => {
-                if ( target == item || target.parentNode == item ) {
-                    hideTabContent()
-                    showTabContent(i)
-                }
-            });
-        }
-    })
-}
-
-// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
-// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
-// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
-// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
-tabs( '.tabs__header' ,'.tabs__header-item', '.tabs__content-item', 'active') 
+ 
 const swiper = new Swiper('.hero__swiper', {
     slidesPerView: 1,
     spaceBetween: 20,
@@ -361,173 +321,7 @@ const swiperTabs = new Swiper(".tabs__swiper", {
             slidesPerView: 5,
         },
     }
-}); 
-const getTemplate = (data = [], placeholder, selectedId) => {
-    let text = placeholder ?? 'placeholder не указан'
-
-    const items = data.map(item => {
-        let cls = ''
-        if (item.id === selectedId) {
-            text = item.value
-            cls = 'selected'
-        }
-        return `
-            <li class="select__item ${cls}" data-type="item" data-id="${item.id}">${item.value}</li>
-        `
-    })
-    return `
-        <input type="hidden" class="hidden__input">
-        <div class="select__backdrop" data-type="backdrop"></div>
-        <div class="select__input" data-type="input">
-            <span data-type="value">${text}</span>
-            <img src="./img/down-arrow.png" alt="arrow" data-type="arrow" class="select__arrow">
-        </div>
-        <div class="select__dropdown">
-            <ul class="select__list">
-                ${items.join('')}
-            </ul>
-        </div>
-    `
-}
-class Select {
-    constructor(selector, options) {
-        this.$el = document.querySelector(selector)
-        this.options = options
-        this.selectedId = options.selectedId
-
-        this.render()
-        this.setup()
-    }
-
-    render() {
-        const { placeholder, data } = this.options
-        this.$el.classList.add('select')
-        this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId)
-    }
-    setup() {
-        this.clickHandler = this.clickHandler.bind(this)
-        this.$el.addEventListener('click', this.clickHandler)
-        this.$arrow = this.$el.querySelector('[data-type="arrow"]')
-        this.$value = this.$el.querySelector('[data-type="value"]')
-    }
-
-    clickHandler(event) {
-        const { type } = event.target.dataset
-        if (type === 'input') {
-            this.toggle()
-        } else if (type === 'item') {
-            const id = event.target.dataset.id
-            this.select(id)
-        }  else if (type === 'backdrop') {
-            this.close()
-        }
-    }
-
-    get isOpen() {
-        return this.$el.classList.contains('open')
-    }
-
-    get current() {
-        return this.options.data.find(item => item.id === this.selectedId)
-    }
-
-    select(id) {
-        this.selectedId = id
-        this.$value.textContent = this.current.value
-
-        this.$el.querySelectorAll(`[data-type="item"]`).forEach( el => el.classList.remove('selected'))
-        this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
-
-        this.options.onSelect ? this.options.onSelect(this.current) : null
-        this.close()
-    }
-
-    toggle() {
-        this.isOpen ? this.close() : this.open()
-    }
-
-    open() {
-        this.$el.classList.add('open')
-        this.$arrow.classList.add('open')
-    }
-
-    close() {
-        this.$el.classList.remove('open')
-        this.$arrow.classList.remove('open')
-    }
-
-    destroy() {
-        this.$el.removeEventListener('click', this.clickHandler)
-        this.$el.innerHTML = ''
-    }
-}
-
-
-// Инициализация плагина
-const select = new Select('#select', {
-    placeholder: 'Выберите элемент',
-    selectedId: '1',
-    data: [
-        {id: '1', value: 'RU'},
-        {id: '2', value: 'EN'},
-    ],
-    onSelect(item) {
-        const input = document.querySelector('.hidden__input')
-        input.value = item.value
-    } 
-})
-
-const selectTwo = new Select('#selectTwo', {
-    placeholder: 'Выберите элемент',
-    selectedId: '1',
-    data: [
-        {id: '1', value: 'RU'},
-        {id: '2', value: 'EN'},
-    ],
-    onSelect(item) {
-        const input = document.querySelector('.hidden__input')
-        input.value = item.value
-    } 
-})
-
-const selectSort = new Select('#selectSort', {
-    placeholder: 'Выберите элемент',
-    selectedId: '1',
-    data: [
-        {id: '1', value: 'по алфавиту (а-я, a-z)'},
-        {id: '2', value: 'по алфавиту (а-я, a-z)'},
-        {id: '3', value: 'по алфавиту (а-я, a-z)'},
-    ],
-    onSelect(item) {
-        const input = document.querySelector('.hidden__input')
-        input.value = item.value
-    } 
-})
-
-const selectMark = new Select('#selectMark', {
-    placeholder: 'Выберите элемент',
-    selectedId: '1',
-    data: [
-        {id: '1', value: 'все'},
-    ],
-    onSelect(item) {
-        const input = document.querySelector('.hidden__input')
-        input.value = item.value
-    } 
-})
-
-const selectCountry = new Select('#selectCountry', {
-    placeholder: 'Выберите элемент',
-    selectedId: '1',
-    data: [
-        {id: '1', value: 'германия'},
-        {id: '2', value: 'корея'},
-    ],
-    onSelect(item) {
-        const input = document.querySelector('.hidden__input')
-        input.value = item.value
-    } 
-})
+});
 
 
 
@@ -551,3 +345,46 @@ function accordion() {
     })
 }
 accordion() 
+
+
+
+// TABS
+
+function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector)
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none'
+        });
+        tab.forEach(item => {
+            item.classList.remove(activeClass)
+        });
+    }
+    function showTabContent(i = 0) {
+       content[i].style.display = display
+       tab[i].classList.add(activeClass)
+    }
+    hideTabContent()
+    showTabContent()
+    header.addEventListener('click', e => {
+        const target = e.target
+        if (target.classList.contains(tabSelector.replace(/\./, '')) || 
+        target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+            tab.forEach((item, i) => {
+                if ( target == item || target.parentNode == item ) {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            });
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs( '.tabs__header' ,'.tabs__header-item', '.tabs__content-item', 'active')
+ 
