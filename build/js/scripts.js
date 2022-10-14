b79519ec661d0bf5b018591e1350ecc217a84b25
abbos <abbosvaliev2003@gmail.com>
2022-10-14 10:54:19 +0500

    feat: add partners page

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -372,6 +372,48 @@ const swiperBlog = new Swiper('.blog__swiper', {
   //     spaceBetween: 30
   //   },
   // }
+});
+
+
+const swiperClients2 = new Swiper('.clients__swiper2', {
+    slidesPerView: 6,
+    spaceBetween: 20,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+    // Responsive breakpoints
+    breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+
+    700: {
+        slidesPerView: 3,
+        spaceBetween: 20
+    },
+
+    992: {
+        slidesPerView: 4,
+        spaceBetween: 20
+    },
+
+    1400: {
+        slidesPerView: 5,
+        spaceBetween: 20
+    },
+  }
 }); 
 // TABS
 
