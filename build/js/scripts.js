b7222e2c567d9c596236a3fd656789d28fa8f9c9
abbos <abbosvaliev2003@gmail.com>
2022-10-29 20:34:54 +0500

    feat: new catalog dropdown

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,69 +1,76 @@
 // Custom Scripts
 // Custom scripts
 
-
 // Мобильное меню бургер
 function burgerMenu() {
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
-  const body = document.querySelector('body')
-  burger.addEventListener('click', () => {
-    if (!menu.classList.contains('active')) {
-      menu.classList.add('active')
-      burger.classList.add('active-burger')
-      body.classList.add('locked')
-    } else {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    }
-  })
-  menu.addEventListener("click", (event) => {
-    if(event.target){
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    }
-  })
-  // Вот тут мы ставим брейкпоинт навбара
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 991.98) {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    }
-  })
-}
-burgerMenu()
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".menu");
+    const body = document.querySelector("body");
+    burger.addEventListener("click", () => {
+        if (!menu.classList.contains("active")) {
+            menu.classList.add("active");
+            burger.classList.add("active-burger");
+            body.classList.add("locked");
+        } else {
+            menu.classList.remove("active");
+            burger.classList.remove("active-burger");
+            body.classList.remove("locked");
+        }
+    });
+    menu.addEventListener("click", (event) => {
+        if (event.target) {
+            menu.classList.remove("active");
+            burger.classList.remove("active-burger");
+            body.classList.remove("locked");
+        }
+    });
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove("active");
+            burger.classList.remove("active-burger");
+            body.classList.remove("locked");
+        }
+    });
 
+
+}
+burgerMenu();
 
 // Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
 function fixedNav() {
-  const nav = document.querySelector('nav')
+    const nav = document.querySelector("nav");
 
-  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-  const breakpoint = 1
-  if (window.scrollY >= breakpoint) {
-    nav.classList.add('fixed__nav')
-  } else {
-    nav.classList.remove('fixed__nav')
-  }
+    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+    const breakpoint = 1;
+    if (window.scrollY >= breakpoint) {
+        nav.classList.add("fixed__nav");
+    } else {
+        nav.classList.remove("fixed__nav");
+    }
 }
-window.addEventListener('scroll', fixedNav)
-
-
-
+window.addEventListener("scroll", fixedNav);
 
 // DROPDOWN CATALOG
+const catalogBtn = document.getElementById("dropdownBtn");
 
+catalogBtn.onclick = function () {
+    catalogBtn.classList.toggle("white");
+    document.getElementById("dropdownMenu").classList.toggle("active-drop");
+    document.body.classList.toggle("lock");
+    catalogBtn.querySelector(".dropdown-btn-close").classList.toggle("active");
+    catalogBtn.querySelector(".dropdown-btn-open").classList.toggle("active");
+    // document.getElementById("bgGray").classList.toggle("bg-gray-active");
 
-// document.getElementById('dropdownBtn').onclick = function() {
-//     document.getElementById('dropdownMenu').classList.toggle('active-drop');
-//     document.getElementById('bgGray').classList.toggle('bg-gray-active');
-//     document.getElementById('body').classList.toggle('lock');
-    
-//     document.getElementById('dropdownBtn').innerHTML = "<img src='./img/close.png'> закрыть";
-// } 
+};
+
+const header = document.querySelector('header');
+const drMenu = document.getElementById("dropdownMenu");
+
+drMenu.style.height = "calc(100vh - " + header.offsetHeight + "px)";
+// 
+
+ 
 // Аккордеон
 function accordion() {
     const items = document.querySelectorAll('.accordion__item-trigger')
